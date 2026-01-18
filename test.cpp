6049eeceb9e06eaf57d1e3b67032732ddb5e9af6
lijinghui <297157262@qq.com>
2026-01-18 18:03:30 +0800

    Test pathch format begin

--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@
 int main(){
 std::cout<<"hello world! lijinghui test branch feature 2"<<std::endl;
 std::cout<<"hello world! lijinghui test branch start Rebase"<<std::endl;
+std::cout<<"hello world! lijinghui test patch format"<<std::endl;
 return 0;
 }
