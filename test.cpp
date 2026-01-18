dccd0381b5292bfa3335e4947ecf10cdc9f9dc0c
lijinghui <297157262@qq.com>
2026-01-18 15:54:57 +0800

    Feature2, before rebase, start

--- test.cpp
+++ test.cpp
@@ -2,6 +2,6 @@
 #include<stdio.h>
 int main(){
 std::cout<<"hello world! lijinghui test branch feature 2"<<std::endl;
-std::cout<<"hello world! lijinghui test branch start feature 2"<<std::endl;
+std::cout<<"hello world! lijinghui test branch start Rebase"<<std::endl;
 return 0;
 }
