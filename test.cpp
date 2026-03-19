37de0d2513a5098b2b2479897e5692362f1977d6
sll-2026 <lijinghui2002@163.com>
2026-03-19 21:00:57 +0800

    Update test.cpp
    second

--- test.cpp
+++ test.cpp
@@ -4,6 +4,6 @@ int main(){
   std::cout<<"hello world! lijinghui test branch feature 2"<<std::endl;
   std::cout<<"hello world! lijinghui test branch start Rebase"<<std::endl;
   std::cout<<"hello world! lijinghui test patch format"<<std::endl;
-  std::cout<<"hello world! shenlingling test patch format"<<std::endl;
+  std::cout<<"hello world! shenlingling + lijinghui test patch format"<<std::endl;
 return 0;
 }
