65c9c34e823566e70e415113b7c711e3cf95e22a
lijinghui <297157262@qq.com>
2026-03-21 15:13:01 +0800

    add FeatureB

--- test.cpp
+++ test.cpp
@@ -6,5 +6,6 @@ int main(){
   std::cout<<"hello world! lijinghui test patch format"<<std::endl;
   std::cout<<"hello world! shen + li test patch format"<<std::endl;
   std::cout<<"hello world! SLL + ljh test patch format"<<std::endl;
+  std::cout<<"FeatureB"<<std::endl;
 return 0;
 }
